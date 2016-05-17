ed8ce63fd27af255b471fb2c4dd9cf546282f176
Craig Michael Bender <Craig.Bender@Gmail.com>
2016-05-17 09:48:47 -0700

    Update JujuPrep.sh

--- JujuPrep.sh
+++ JujuPrep.sh
@@ -262,13 +262,13 @@ clear
 		printf "${BW}┗━ Addomg ${BO}Environments Stanza${GR} to ${BW}environments.yaml ${RT}"
 		printf "environments:\n" >> ~/.juju/environments.yaml;QStatusChk
 		printf "${BW}┗━ Changing ${BO}Environment Name${GR} to ${BW}${ENV_NAME} ${RT}"
-		printf "\t${ENV_NAME}\n" >> ~/.juju/environments.yaml;QStatusChk
+		printf "${ENV_NAME}\n" >> ~/.juju/environments.yaml;QStatusChk
 		printf "${BW}┗━ Changing ${BO}Environment Type${GR} to ${BW}${ENV_TYPE} ${RT}"
-		printf "\t\ttype: ${ENV_TYPE}\n" >> ~/.juju/environments.yaml;QStatusChk
+		printf "type: ${ENV_TYPE}\n" >> ~/.juju/environments.yaml;QStatusChk
 		[[ -z $AWS_ACCESS_KEY_ID ]] && printf "${BW}┗━ Changing ${BC}access-key:${GR} to ${BW}${ACCESS_KEY} ${RT}"
-		[[ -z $AWS_ACCESS_KEY_ID ]] && printf "\t\taccess-key: ${ACCESS_KEY}\n" >> ~/.juju/environments.yaml;QStatusChk
+		[[ -z $AWS_ACCESS_KEY_ID ]] && printf "access-key: ${ACCESS_KEY}\n" >> ~/.juju/environments.yaml;QStatusChk
 		[[ -z $AWS_SECRET_ACCESS_KEY ]] && printf "${BW}┗━ Changing ${BC}secret-key${GR} to ${BW}${SECRET_KEY} ${RT}"
-		[[ -z $AWS_SECRET_ACCESS_KEY ]] && printf "\t\tsecret-key: ${SECRET_KEY}\n" >> ~/.juju/environments.yaml;QStatusChk
+		[[ -z $AWS_SECRET_ACCESS_KEY ]] && printf "secret-key: ${SECRET_KEY}\n" >> ~/.juju/environments.yaml;QStatusChk
 		chown $USER:$USER ~/.juju/environments.yaml
 		printf "\n\n${RCW}  Canonical Juju is ready for bootstrapping  ${RT}\n"
 		printf "${BW}┗━ Bootstrapping ${BO}Juju Environment: ${BW}${ENV_NAME} ${RT}"
